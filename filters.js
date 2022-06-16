07c3401c5196d4d0e0ecac2140e082cc0cae5883
Luke Bonaccorsi <luke@lukeb.co.uk>
2022-06-16 11:55:47 +0100

    Add prettier and format files

--- filters.js
+++ filters.js
@@ -2,82 +2,82 @@ const { isRedirect } = require("node-fetch");
 const { URL } = require("url");
 
 const allowedTypes = {
-	likes: ["like-of"],
-	reposts: ["repost-of"],
-	comments: ["mention-of", "in-reply-to"],
+  likes: ["like-of"],
+  reposts: ["repost-of"],
+  comments: ["mention-of", "in-reply-to"],
 };
 
 function stripTrailingSlashes(str) {
-	let i = str.length;
-	while (str[--i] === "/");
-	return str.slice(0, i + 1);
+  let i = str.length;
+  while (str[--i] === "/");
+  return str.slice(0, i + 1);
 }
 
 module.exports = ({
-	maxContentLength = 280,
-	truncationMarker = '&hellip; <span class="webmention__truncated">Truncated</span>',
-	types = allowedTypes,
+  maxContentLength = 280,
+  truncationMarker = '&hellip; <span class="webmention__truncated">Truncated</span>',
+  types = allowedTypes,
 }) => {
-	function filterWebmentions(webmentions, page) {
-		const pageUrl = new URL(page, "https://lukeb.co.uk");
+  function filterWebmentions(webmentions, page) {
+    const pageUrl = new URL(page, "https://lukeb.co.uk");
 
-		const flattenedAllowedTypes = Object.values(types).flat();
+    const flattenedAllowedTypes = Object.values(types).flat();
 
-		return webmentions
-			.filter((mention) => {
-				const target = new URL(mention["wm-target"]);
-				const regex = new RegExp(`${stripTrailingSlashes(pageUrl.pathname)}$`);
-				return regex.test(stripTrailingSlashes(target.pathname));
-			})
-			.filter(
-				(entry) => !!entry.author && (!!entry.author.name || entry.author.url)
-			)
-			.filter((mention) =>
-				flattenedAllowedTypes.includes(mention["wm-property"])
-			);
-	}
+    return webmentions
+      .filter((mention) => {
+        const target = new URL(mention["wm-target"]);
+        const regex = new RegExp(`${stripTrailingSlashes(pageUrl.pathname)}$`);
+        return regex.test(stripTrailingSlashes(target.pathname));
+      })
+      .filter(
+        (entry) => !!entry.author && (!!entry.author.name || entry.author.url)
+      )
+      .filter((mention) =>
+        flattenedAllowedTypes.includes(mention["wm-property"])
+      );
+  }
 
-	function clean(entry) {
-		if (entry.content) {
-			entry.content.value =
-				maxContentLength > 0 && entry.content.text.length > maxContentLength
-					? `${entry.content.text.substr(
-							0,
-							maxContentLength
-					  )}${truncationMarker}`
-					: entry.content.text;
-		}
-		return entry;
-	}
+  function clean(entry) {
+    if (entry.content) {
+      entry.content.value =
+        maxContentLength > 0 && entry.content.text.length > maxContentLength
+          ? `${entry.content.text.substr(
+              0,
+              maxContentLength
+            )}${truncationMarker}`
+          : entry.content.text;
+    }
+    return entry;
+  }
 
-	function count(webmentions, page) {
-		return filterWebmentions(webmentions, page).length;
-	}
+  function count(webmentions, page) {
+    return filterWebmentions(webmentions, page).length;
+  }
 
-	function mentions(webmentions, page) {
-		const cleanedWebmentions = filterWebmentions(webmentions, page)
-			.sort(
-				(a, b) =>
-					new Date(a.published || a["wm-received"]) -
-					new Date(b.published || b["wm-received"])
-			)
-			.map(clean);
+  function mentions(webmentions, page) {
+    const cleanedWebmentions = filterWebmentions(webmentions, page)
+      .sort(
+        (a, b) =>
+          new Date(a.published || a["wm-received"]) -
+          new Date(b.published || b["wm-received"])
+      )
+      .map(clean);
 
-		const returnedWebmentions = {
-			total: cleanedWebmentions.length,
-		};
+    const returnedWebmentions = {
+      total: cleanedWebmentions.length,
+    };
 
-		Object.keys(types).map((type) => {
-			returnedWebmentions[type] = cleanedWebmentions.filter((mention) =>
-				types[type].includes(mention["wm-property"])
-			);
-		});
+    Object.keys(types).map((type) => {
+      returnedWebmentions[type] = cleanedWebmentions.filter((mention) =>
+        types[type].includes(mention["wm-property"])
+      );
+    });
 
-		return returnedWebmentions;
-	}
+    return returnedWebmentions;
+  }
 
-	return {
-		count,
-		mentions,
-	};
+  return {
+    count,
+    mentions,
+  };
 };
